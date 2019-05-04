2042ca9e1c46b8f3c564b8a007516a757b8dc4d0
Rori Hinds <r_ori@hotmail.co.uk>
2019-05-04 23:20:26 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -15,7 +15,7 @@ def speak_to_grandma(string)
   
   
   if string ==  'I LOVE YOU GRANDMA!'
-    return 'NO, NOT SINCE 1938!'
+    return 'I LOVE YOU TOO PUMPKIN!'
   elsif string == string.upcase
     return 'NO, NOT SINCE 1938!'
   else 
